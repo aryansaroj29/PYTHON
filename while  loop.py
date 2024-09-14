6e4b0f12d6399d8737683fa76038a91ca6f3350f
aryansaroj29 <88679634+aryansaroj29@users.noreply.github.com>
2024-09-14 14:24:52 +0530

    Update while  loop.py

--- while  loop.py
+++ while  loop.py
@@ -3,3 +3,9 @@ while (i <= 3):
   print(i)
   i = i + 1
 print("Done with the loop") 
+
+i = int(input("Enter the number:"))
+while (i <=38):
+  i = int(input("Enter the numnber:"))
+  print(i)
+print("Done with the loop") 
