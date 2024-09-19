b1b70874e1e9eaf634e6b8cce6ae26f74dd67d9e
aryansaroj29 <88679634+aryansaroj29@users.noreply.github.com>
2024-09-19 20:59:35 +0530

    Update and rename list index.py to list.py

--- list.py
+++ list.py
@@ -1,3 +1,4 @@
+POSITIVE INDEX - 
 marks = [3, 5, 6, "Aryan", True]     #List Function any Datatupes use str, int, boolean
 print(marks)
 print(type(marks))
@@ -6,3 +7,9 @@ print(marks[1])
 print(marks[2])
 print(marks[3])
 print(marks[4])
+
+NEGATIVE INDEX - 
+print(marks[-3]) # Negative index
+print(marks[len(marks)-3])  # Positive index
+print(marks[5-3]) # Positive index
+print(marks[2]) # Positive index
