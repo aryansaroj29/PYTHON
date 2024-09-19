0a27f2e10440a07142b7a8c17accd94c1c94a66e
aryansaroj29 <88679634+aryansaroj29@users.noreply.github.com>
2024-09-19 20:39:53 +0530

    Update list.py

--- list.py
+++ list.py
@@ -1,6 +1,8 @@
-marks = [3, 5, 6]
+marks = [3, 5, 6, "Aryan", True]     #List Function any Datatupes use str, int, boolean
 print(marks)
 print(type(marks))
 print(marks[0])
 print(marks[1])
 print(marks[2])
+print(marks[3])
+print(marks[4])
