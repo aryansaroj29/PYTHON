065f7dbf342231885fa1e4cb11d46f57fd6ba3c3
aryansaroj29 <88679634+aryansaroj29@users.noreply.github.com>
2024-09-21 12:22:47 +0530

    Create Basic.py

---
+++ Basic.py
@@ -0,0 +1,83 @@
+
+1. print("Hello World").
+
+2. print(17).......Numeric double inverted ("") is it okay are not using
+   print(17+13) (17-13) (17*13) (17/13).
+
+3. print("Hey I am a good boy\nand this viewer also is a good boy/girl")..........\n are using to line changing.
+
+4. # Hey Aryan, please dont remove this line
+   # Author: Aryan
+   # Course: 100Daysofcode........(# USING FOR COMMENTS)......SHORTCUT KEY [CRTL/].
+
+5. Multi-Line Comments Show that ('''      '''), ("""      """).
+
+6. print('Hey I am\' a \"good boy\"\n and this viewer also is a good boy/girl').............('Hey......am\'a \"good boy\"\n.........').
+
+7. print("Hey", 5,  7, sep="~", end="001\n")
+print("Aryan").........................(sep=" ", end=" ").
+
+8. a = 1
+   b = True
+   c = "Harry"
+   d = None
+
+  print(a)                                       -->                       (1)   
+  print(b)                                       -->                      (True)
+  a1 = 9 
+  print (a + a1)                                 -->                       (10)
+  print("The type of a is", type (a))            -->            {The type of a is <class 'int'>}
+  print("The type of b is", type (b))            -->             {The type of b is <class 'bool'>}
+  print("The type of c is", type (c))            -->             {The type of c is <class 'str'>}
+
+  1.Numeric data: int, float, complex
+    int: 3, -8, 0
+    float: 7.349, -9.0, 0.0000001
+    complex: 6 + 2i
+
+  2.Text data: str
+    str: "Hello World!!!", "Python Programming"
+
+  3.Boolean data:
+    Boolean data consists of values True or False.
+
+  4.Sequenced data: list, tuple (Mutable changes ho sakta hai)
+    
+    {LIST}
+    list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+    print(list1)
+    Output: [8, 2.3, [-4, 5], ['apple', 'banana']]
+    
+    {TUPLE}
+    tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+    print(tuple1) 
+    Output: (('parrot', 'sparrow'), ('Lion', 'Tiger'))
+
+  5.Mapped data: dictionary is callled a key value pair
+   dict1 = {"name":"Sakshi", "age":20, "canVote":True}
+   print(dict1)
+   Output: {'name': 'Sakshi', 'age': 20, 'canVote': True}
+
+9.    Operator	      Operator Name      Example
+         +	        Addition	  15+7
+         -	       Subtraction    	  15-7
+         *	      Multiplication	  5*7
+         **	       Exponential	  5**3
+         /	        Division	  5/3
+         %	        Modulus	          15%7
+         //	      Floor Division	  15//7
+
+10.  a = 50
+     b = 3
+
+     print("The value of", a, "+", b, "is:", a + b)
+     print("The Value of", a, "-", b, "is:", a - b)
+     print("The value of", a, "*", b, "is:", a * b)
+     print("The Value of", a, "/", b, "is:", a / b)
+     print("The value of", a, "**", b, "is:", a ** b)
+     print("The Value of", a, "//", b, "is:", a // b)
+     print("The value of", a, "%", b, "is:", a % b)
+
+
+
+
