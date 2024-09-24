97d63963960f67c4eb0443017b0cd3a8f0058cfb
aryansaroj29 <88679634+aryansaroj29@users.noreply.github.com>
2024-09-24 22:31:51 +0530

    Create KBC GAME.py

---
+++ KBC GAME.py
@@ -0,0 +1,26 @@
+print("Let's Play Kaun Banega Crorepati \nYe Raha Aapka Question aapke computer screen par")
+
+Question = ("\nWhich of the following social media platforms is famous for its disappearing photos and videos feature?")
+
+print(Question)
+
+Option = ("Option are:\n A Snapchat\n B TikTok \n C WhatsApp\n D Instagram")
+print(Option)
+
+Answer = input("Enter Your Answer:")
+
+if Answer == "A":
+  print("Aapka Sahi Jawab")
+  print("Pure 7 crore") 
+  
+elif Answer == "B":
+  print("Aapka Galat Jawab")
+
+elif Answer == "C":
+  print("Aapka Galat Jawab")
+
+elif Answer == "D":
+  print("Aapka Galat Jawab")
+
+else:
+  print("Aapka Galat Jawab")
