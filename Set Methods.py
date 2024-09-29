b337378b2c30e9c64696d1e8cabe17cb082304e2
aryansaroj29 <88679634+aryansaroj29@users.noreply.github.com>
2024-09-30 02:20:26 +0530

    Update Set Methods.py

--- Set Methods.py
+++ Set Methods.py
@@ -59,4 +59,78 @@ cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
 cities2 = {"Seoul", "Kabul", "Delhi"}
 print(cities.difference(cities2))
 
+# isdisjoint():
 
+cities1 = {"Mumbai", "Delhi", "Bangalore", "Chennai"}
+cities2 = {"Delhi1", "Kolkata", "Hyderabad", "Mumbai1" }
+print(cities1.isdisjoint(cities2))
+
+# issuperset():
+
+cities  = {"MUMBAI", "DELHI", "BANGLORE", "KOLKATA" }
+cities2 = {"KOLKATA", "HYDREABAD", "CHENNAI"}
+print(cities.issuperset(cities2))
+citites3 = {"MUMBAI", "DELHI", "BANGLORE"}
+print(cities.issuperset(citites3))
+
+# issubset():
+
+cities  = {"MUMBAI", "DELHI", "BANGLORE", "KOLKATA"}
+cities2 = {"KOLKATA", "HYDREABAD", "CHENNAI"}
+print(cities.issubset(cities2))
+cities3 = {"MUMBAI", "DELHI", "BANGLORE",}
+print(cities3.issubset(cities))
+
+# add():
+
+cities  = {"MUMBAI", "DELHI", "BANGLORE", "KOLKATA"}
+cities.add("CHENNAI")
+print(cities)
+
+# update():
+
+cities  = {"MUMBAI", "DELHI", "BANGLORE"}
+cities2 = {"KOLKATA", "HYDREABAD", "CHENNAI"}
+cities.update(cities2)
+print(cities)
+
+# remove():
+
+cities  = {"MUMBAI", "DELHI", "BANGLORE", "KOLKATA"}
+cities.remove("MUMBAI")
+print(cities)
+
+# discard():
+
+cities  = {"MUMBAI", "DELHI", "BANGLORE", "KOLKATA"}
+cities.discard("MUMBAI12")
+print(cities)
+
+# pop():
+
+cities  = {"MUMBAI", "DELHI", "BANGLORE", "KOLKATA"}
+item= cities.pop()
+print(cities)
+print(item)
+
+# del():
+
+cities   = {"MUMBAI", "DELHI", "BANGLORE", "KOLKATA"}
+cities2  = {"MUMBAI", "DELHI", "BANGLORE", "KOLKATA"}
+del cities
+print(cities2)
+
+# clear():
+
+cities   = {"MUMBAI", "DELHI", "BANGLORE", "KOLKATA"}
+cities.clear()
+print(cities)
+
+# Check if item exists:
+
+info = {"Aryan", 19 , False, 19.5}
+if "Aryan" in info:
+  print("Aryan is present")
+else:
+  print("Aryan is absent")
+  
